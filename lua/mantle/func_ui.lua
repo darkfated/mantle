69975606c41845a97e76a421e334ef9f19ad0497
Dark Fated <darkfated@icloud.com>
2023-10-04 22:19:58 +0300

    New ui functions

--- lua/mantle/func_ui.lua
+++ lua/mantle/func_ui.lua
@@ -32,6 +32,45 @@ local function create_ui_func()
 			surface.DrawTexturedRect(-x, -y, scrw, scrh)
 		end
 	end
+
+	local list_gradient = {
+		surface.GetTextureID('gui/gradient_up'),
+		surface.GetTextureID('gui/gradient_down')
+	}
+
+	function Mantle.func.gradient(_x, _y, _w, _h, direction, color_shadow)
+		draw.TexturedQuad{
+			texture = list_gradient[direction],
+			color = color_shadow,
+			x = _x,
+			y = _y,
+			w = _w,
+			h = _h
+		}
+	end
+
+	function Mantle.func.sound(snd)
+		surface.PlaySound(snd or 'UI/buttonclickrelease.wav')
+	end
+
+	Mantle.func.w_save = {}
+	Mantle.func.h_save = {}
+	
+	function Mantle.func.w(w)
+		if !Mantle.func.w_save[w] then
+			Mantle.func.w_save[w] = w / 1920 * scrw
+		end
+	
+		return Mantle.func.w_save[w]
+	end
+	
+	function Mantle.func.h(h)
+		if !Mantle.func.h_save[h] then
+			Mantle.func.h_save[h] = h / 1920 * scrh
+		end
+	
+		return Mantle.func.h_save[h]
+	end
 end
 
 local function create_vgui()
