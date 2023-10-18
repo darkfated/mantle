369bdfe91c8b4d30d5af88ee4f675c4e7bdce771
Dark Fated <darkfated@icloud.com>
2023-10-19 02:41:32 +0300

    ui button: added custom hover color

--- lua/mantle/func_ui.lua
+++ lua/mantle/func_ui.lua
@@ -129,7 +129,7 @@ local function create_vgui()
         end
     end
 
-    function Mantle.ui.btn(s, icon, icon_size, btn_color, btn_radius, off_grad_bool)
+    function Mantle.ui.btn(s, icon, icon_size, btn_color, btn_radius, off_grad_bool, btn_color_hov)
         s:SetTall(32)
         s.Paint = function(self, w, h)
             if !self.btn_text then
@@ -137,7 +137,9 @@ local function create_vgui()
                 self:SetText('')
             end
 
-            draw.RoundedBox(btn_radius and btn_radius or 6, 0, 0, w, h, self:IsHovered() and Mantle.color.button_hovered or (btn_color and btn_color or Mantle.color.button))
+            print(btn_color_hov)
+
+            draw.RoundedBox(btn_radius and btn_radius or 6, 0, 0, w, h, self:IsHovered() and (btn_color_hov and btn_color_hov or Mantle.color.button_hovered) or (btn_color and btn_color or Mantle.color.button))
 
             if !off_grad_bool then
                 Mantle.func.gradient(0, 0, w, h, 1, Mantle.color.button_shadow)
