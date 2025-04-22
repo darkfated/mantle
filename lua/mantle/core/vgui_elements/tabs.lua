b6d38bf99b4b33f62e70bd550366ce430c4e5a7e
darkfated <49955245+darkfated@users.noreply.github.com>
2025-04-22 08:51:21 +0300

    Улучшение визуала вкладок

--- lua/mantle/core/vgui_elements/tabs.lua
+++ lua/mantle/core/vgui_elements/tabs.lua
@@ -26,12 +26,14 @@ function PANEL:AddTab(name, pan, icon)
     self:Rebuild()
 end
 
+local color_btn_hovered = Color(255, 255, 255, 10)
+
 function PANEL:Rebuild()
     self.panel_tabs:Clear()
 
     for id, tab in ipairs(self.tabs) do
         local btnTab = vgui.Create('Button', self.panel_tabs)
-        btnTab:SetTall(30)
+        btnTab:SetTall(34)
         btnTab:Dock(TOP)
         btnTab:DockMargin(0, 0, 0, 6)
         btnTab:SetText('')
@@ -46,24 +48,28 @@ function PANEL:Rebuild()
         local color_draw = Color(255, 255, 255)
 
         btnTab.Paint = function(s, w, h)
+            if s:IsHovered() then
+                draw.RoundedBox(8, 0, 0, w, h, color_btn_hovered)
+            end
+
             local color_target = Color(255, 255, 255)
 
             if self.active_id == id then
                 color_target = Color(Mantle.color.theme.r, Mantle.color.theme.g, Mantle.color.theme.b)
             end
 
-            local r = Lerp(FrameTime() * 10, color_draw.r, color_target.r)
-            local g = Lerp(FrameTime() * 10, color_draw.g, color_target.g)
-            local b = Lerp(FrameTime() * 10, color_draw.b, color_target.b)
+            local r = Lerp(FrameTime() * 16, color_draw.r, color_target.r)
+            local g = Lerp(FrameTime() * 16, color_draw.g, color_target.g)
+            local b = Lerp(FrameTime() * 16, color_draw.b, color_target.b)
             color_draw = Color(r, g, b)
 
-            draw.SimpleText(tab.name, 'Fated.18', 41, h * 0.5 + 1, color_black, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
-            draw.SimpleText(tab.name, 'Fated.18', 40, h * 0.5, color_draw, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+            draw.SimpleText(tab.name, 'Fated.20', 41, h * 0.5, color_black, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+            draw.SimpleText(tab.name, 'Fated.20', 40, h * 0.5 - 1, color_draw, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
 
             if tab.icon then
                 surface.SetDrawColor(color_draw)
                 surface.SetMaterial(tab.icon)
-                surface.DrawTexturedRect(0, 0, 30, 30)
+                surface.DrawTexturedRect(2, 2, 30, 30)
             else
                 draw.RoundedBox(6, 0, 0, 30, 30, color_draw)
             end
@@ -73,7 +79,8 @@ end
 
 function PANEL:PerformLayout(w, h)
     self.panel_tabs:Dock(LEFT)
-    self.panel_tabs:SetWide(180)
+    self.panel_tabs:DockMargin(0, 0, 4, 0)
+    self.panel_tabs:SetWide(190)
 
     self.content:Dock(FILL)
 end
