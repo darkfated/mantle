cacf7bc4049a6a73093690283cd68eb4f89f6947
darkfated <darkfated@icloud.com>
2025-06-04 16:37:12 +0300

    Исправление цвета sp

--- lua/mantle/core/vgui_elements/scrollpanel.lua
+++ lua/mantle/core/vgui_elements/scrollpanel.lua
@@ -5,12 +5,12 @@ function PANEL:Init()
     vbar:SetWide(6)
     vbar:SetHideButtons(true)
     vbar.Paint = function(_, w, h)
-        RNDX.Draw(16, 0, 0, w, h, Mantle.color.spacer, RNDX.SHAPE_IOS)
+        RNDX.Draw(32, 0, 0, w, h, Mantle.color.sp)
 
         self.pnlCanvas:DockPadding(0, 0, m_bNoSizing and 0 or 6, 0)
     end
     vbar.btnGrip.Paint = function(_, w, h)
-        RNDX.Draw(16, 0, 0, w, h, Mantle.color.theme, RNDX.SHAPE_IOS)
+        RNDX.Draw(32, 0, 0, w, h, Mantle.color.theme)
     end
 end
 
