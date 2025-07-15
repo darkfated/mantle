da568f52ba1b99355731bc5dfe2b49dc5fe2fa12
darkfated <darkfated@icloud.com>
2025-07-15 08:06:07 +0300

    Улучшение slider

--- lua/mantle/core/vgui_elements/slidebox.lua
+++ lua/mantle/core/vgui_elements/slidebox.lua
@@ -93,59 +93,50 @@ function PANEL:Paint(w, h)
     local barY = 32
     local barH = 6
     local barR = barH/2
-    local handleR = 7
-    local handleW, handleH = handleR*2, handleR*2
+    local handleW, handleH = 14, 14
+    local handleR = handleH/2
     local textFont = 'Fated.18'
     local minmaxFont = 'Fated.14'
     local valueFont = 'Fated.16'
     local minmaxPadY = 12
 
-    -- Текст сверху (с большим отступом)
+    -- Текст сверху
     draw.SimpleText(self.text, textFont, padX, padTop, color_white)
 
     -- Линия
-    local barStart = padX + handleR
-    local barEnd = w - padX - handleR
+    local barStart = padX + handleW/2
+    local barEnd = w - padX - handleW/2
     local barW = barEnd - barStart
     local progress = (self.value - self.min_value) / (self.max_value - self.min_value)
     local activeW = math.Clamp(barW * progress, 0, barW)
 
-    -- Фон линии
+    -- Тень под линией
     if Mantle.ui.convar.depth_ui then
-        RNDX.DrawShadows(16, barStart, barY, barW, barH, Mantle.color.window_shadow, 5, 20, RNDX.SHAPE_IOS)
+        RNDX.DrawShadows(barR, barStart, barY, barW, barH, Mantle.color.window_shadow, 5, 20)
     end
-    RNDX.Draw(16, barStart, barY, barW, barH, Mantle.color.focus_panel)
-    RNDX.Draw(16, barStart, barY, barW, barH, Mantle.color.button_shadow)
-
+    -- Фон линии
+    RNDX.Draw(barR, barStart, barY, barW, barH, Mantle.color.focus_panel)
+    RNDX.Draw(barR, barStart, barY, barW, barH, Mantle.color.button_shadow)
     -- Активная линия
-    RNDX.Draw(16, barStart, barY, activeW, barH, Mantle.color.theme)
+    RNDX.Draw(barR, barStart, barY, self.smoothPos, barH, Mantle.color.theme)
 
-    -- Маленький шарик-ручка (идеально круглый)
+
     self.smoothPos = Lerp(FrameTime() * 12, self.smoothPos or 0, activeW)
     local handleX = barStart + self.smoothPos
     local handleY = barY + barH/2
-    local handleCol = (self.dragging or self.hover) and Mantle.color.theme or Mantle.color.button
-    surface.SetDrawColor(handleCol)
-    draw.NoTexture()
-    draw.Circle(handleX, handleY, handleR, 32)
-    surface.SetDrawColor(color_white)
-    draw.Circle(handleX, handleY, handleR-2, 32)
+
+    -- Тень под ручкой
+    RNDX.DrawShadows(handleR, handleX - handleW/2, handleY - handleH/2, handleW, handleH, Mantle.color.window_shadow, 3, 10)
+
+    -- Ручка
+    RNDX.Draw(handleR, handleX - handleW/2, handleY - handleH/2, handleW, handleH, color_white)
 
     -- Значение справа от линии
-    draw.SimpleText(self.value, valueFont, barEnd + handleR + 4, barY + barH/2, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
+    draw.SimpleText(self.value, valueFont, barEnd + handleW/2 + 4, barY + barH/2, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
 
     -- min/max под линией
     draw.SimpleText(self.min_value, minmaxFont, barStart, barY + barH + minmaxPadY - 4, Mantle.color.gray, TEXT_ALIGN_LEFT)
     draw.SimpleText(self.max_value, minmaxFont, barEnd, barY + barH + minmaxPadY - 4, Mantle.color.gray, TEXT_ALIGN_RIGHT)
-end
-
-function draw.Circle(x, y, radius, seg)
-    local cir = {}
-    for i = 0, seg do
-        local a = math.rad((i / seg) * -360)
-        table.insert(cir, {x = x + math.sin(a) * radius, y = y + math.cos(a) * radius})
-    end
-    surface.DrawPoly(cir)
 end
 
 function PANEL:OnMousePressed(mcode)
