2c318c8dfa0cef73c1dffa51705f96f7c5301c46
darkfated <darkfated@icloud.com>
2026-01-06 01:05:58 +0300

    Удаление старых тем

--- lua/mantle/core/vgui.lua
+++ lua/mantle/core/vgui.lua
@@ -1,31 +1,13 @@
-CreateClientConVar('mantle_depth_ui', 1, true, false)
 CreateClientConVar('mantle_theme', 'dark', true, false)
-CreateClientConVar('mantle_blur', 1, true, false)
 
 Mantle.ui = {
     convar = {
-        depth_ui = GetConVar('mantle_depth_ui'):GetBool(),
-        theme = GetConVar('mantle_theme'):GetString(),
-        blur = GetConVar('mantle_blur'):GetBool()
+        theme = GetConVar('mantle_theme'):GetString()
     }
 }
 
 local themeMap = {
-    dark = Mantle.color_dark,
-    dark_mono = Mantle.color_dark_mono,
-    graphite = Mantle.color_graphite,
-    light = Mantle.color_light,
-    blue = Mantle.color_blue,
-    red = Mantle.color_red,
-    green = Mantle.color_green,
-    orange = Mantle.color_orange,
-    purple = Mantle.color_purple,
-    coffee = Mantle.color_coffee,
-    ice = Mantle.color_ice,
-    wine = Mantle.color_wine,
-    violet = Mantle.color_violet,
-    moss = Mantle.color_moss,
-    coral = Mantle.color_coral
+    dark = Mantle.color_dark
 }
 
 local function isColor(v)
@@ -92,15 +74,7 @@ end
 
 ApplyInitialTheme()
 
-cvars.AddChangeCallback('mantle_depth_ui', function(_, _, newValue)
-    Mantle.ui.convar.depth_ui = newValue == '1'
-end)
-
 cvars.AddChangeCallback('mantle_theme', function(_, _, newValue)
     Mantle.ui.convar.theme = newValue
     startThemeTransition(newValue)
-end)
-
-cvars.AddChangeCallback('mantle_blur', function(_, _, newValue)
-    Mantle.ui.convar.blur = newValue == '1'
 end)
