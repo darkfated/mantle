33df62f39799b6d3e98f098c6856398478d7b262
darkfated <darkfated@icloud.com>
2026-08-27 00:20:41 +0300

    Убрано не связанное с названием PR

--- lua/mantle/core/vgui_elements/entry.lua
+++ lua/mantle/core/vgui_elements/entry.lua
@@ -1,67 +1,28 @@
 local PANEL = {}
-AccessorFunc(PANEL, "m_iMaxLength", "MaxLength", FORCE_NUMBER)
-AccessorFunc(PANEL, "m_bPasswordMode", "Password", FORCE_BOOL)
-AccessorFunc(PANEL, "m_sPasswordSymbol", "PasswordSymbol", FORCE_STRING)
 
 local HEIGHT = 32
 local RADIUS = 12
 local TITLE_H = 18
 local TITLE_GAP = 6
 
-local function mul_str(str, n)
-    if not isstring(str) or not isnumber(n) then return "" end
-    local needed = ""
-    for _ = 1, n do needed = needed .. str end
-    return needed
-end
-
 function PANEL:Init()
     self:DockMargin(8, 8, 8, 8)
-
     self.title = nil
     self.placeholder = Mantle.lang.get('mantle', 'entry_default_placeholder')
-
     self:SetTall(HEIGHT)
-    self:SetTooltipPanelOverride("MantleTooltip")
-    self:SetMaxLength(-1)
-    self:SetPassword(false)
-    self:SetPasswordSymbol("*")
+    self.action = function() end
 
     self.font = 'Fated.18'
     self._focusLerp = 0
     self._textOffset = 0
     self._caretSize = 2
+    self._caretColor = Mantle.color.text:Copy()
 
     self.textEntry = vgui.Create('DTextEntry', self)
     self.textEntry:Dock(FILL)
     self.textEntry:SetText('')
     self.textEntry.OnLoseFocus = function(s)
-        self:OnEditingDone(s:GetValue())
-    end
-    self.textEntry.OnEnter = function(_, strValue)
-        self:OnEditingDone(strValue)
-    end
-    self.textEntry.OnChange = function(s)
-        self:OnChange(s:GetValue())
-    end
-    self.textEntry.OnValueChange = function(_, strValue)
-        self:OnEditingDone(strValue)
-    end
-    self.textEntry.OnKeyCode = function(_, numKeyCode)
-        self:OnKeyCode(numKeyCode)
-    end
-    self.textEntry.CheckNumeric = function(s, strValue)
-        if not s:GetNumeric() then return true end
-        return string.find(strAllowedNumericCharacters, strValue, 1, true) ~= nil
-    end
-    self.textEntry.AllowInput = function(s, strValue)
-        local max_len = self:GetMaxLength()
-        local len = utf8.len(s:GetValue())
-        if max_len > 0 and len > max_len then return true end
-        return isfunction(self.AllowInput) and self:AllowInput(strValue) or nil
-    end
-    self.textEntry.OnLoseFocus = function(s)
-        self:OnEditingDone(s:GetValue())
+        self.action(s:GetValue())
     end
     self.textEntry.Paint = nil
     self.textEntry.PaintOver = function(s, w, h)
@@ -88,8 +49,7 @@ function PANEL:_paintEntry(s, w, h)
         :Draw()
     end
 
-    local value = self:GetValue() or ''
-    value = self:GetPassword() and mul_str(self:GetPasswordSymbol(), #value) or value
+    local value = self:GetValue()
     surface.SetFont(self.font)
     local padding = 6
     local availableW = w - padding * 2
@@ -108,18 +68,10 @@ function PANEL:_paintEntry(s, w, h)
     draw.SimpleText(text, self.font, padding - self._textOffset, h * 0.5, col, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER)
 
     if s:IsEditing() then
-        local caret = #value
-        local before_caret = string.sub(value, 1, caret)
-
-        surface.SetFont(self.font)
-        local caret_x = surface.GetTextSize(before_caret)
-
-        if not s.caret_col then s.caret_col = Mantle.color.text:Copy() end
-        s.caret_col.a = math.abs(math.floor(255 * math.sin(CurTime() * 3.2)))
-        surface.SetDrawColor(s.caret_col:Unpack())
+        self._caretColor.a = 255 * math.abs(math.sin(CurTime() * 3.2))
+        surface.SetDrawColor(self._caretColor:Unpack())
+        local caret_x = surface.GetTextSize(string.sub(value, 1, #value))
         surface.DrawRect(padding - self._textOffset + caret_x, 5, self._caretSize, h - 10)
-    else
-        s.caret_col = nil
     end
 end
 
@@ -140,74 +92,16 @@ function PANEL:SetTitle(title)
     end
 end
 
-function PANEL:RemoveTitle()
-    if IsValid(self.titlePanel) then
-        self.titlePanel:Remove()
-        self.title = nil
-        self:SetTall(HEIGHT)
-    end
-end
-
 function PANEL:SetPlaceholder(placeholder)
     self.placeholder = placeholder
 end
 
-function PANEL:GetPlaceholder()
-    return self.placeholder
+function PANEL:GetValue()
+    return self.textEntry:GetText()
 end
 
 function PANEL:SetValue(value)
     self.textEntry:SetValue(value)
-    self.textEntry:SetCaretPos(#value)
-end
-
-function PANEL:SetText(txt)
-    self.textEntry:SetText(txt)
-    self.textEntry:SetCaretPos(#txt)
-end
-
-function PANEL:GetValue()
-    return self.textEntry:GetValue()
-end
-PANEL.GetText = PANEL.GetValue
-
-function PANEL:GetFloat()
-    return tonumber(self:GetValue())
-end
-PANEL.GetNumber = PANEL.GetFloat
-
-function PANEL:GetInt()
-    local v = self:GetFloat()
-    return isnumber(v) and math.floor(v) or nil
-end
-
-function PANEL:GetUSInt()
-    local v = self:GetInt()
-    return isnumber(v) and math.abs(v) or nil
-end
-
-function PANEL:TogglePassword()
-    self:SetPassword(not self:GetPassword())
-end
-
-function PANEL:SetNumeric( bool )
-    self.textEntry:SetNumeric(bool)
-end
-
-function PANEL:GetNumeric()
-    return self.textEntry:GetNumeric()
-end
-
-function PANEL:OnEditingDone( strValue )
-end
-
-function PANEL:OnChange( strValue )
-end
-
-function PANEL:OnKeyCode( numKeyCode )
-end
-
-function PANEL:AllowInput( strValue )
 end
 
 vgui.Register('MantleEntry', PANEL, 'EditablePanel')
